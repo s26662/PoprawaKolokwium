d0418a5e65755b40a410a8ad69d30755f862b469
olafc <s26662@pjwstk.edu.pl>
2025-06-21 08:36:46 +0200

    Added Tabels

--- WebApplication1/WebApplication1/DAL/ExamDbContext.cs
+++ WebApplication1/WebApplication1/DAL/ExamDbContext.cs
@@ -1,10 +1,14 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Model;
+using Task = WebApplication1.Model.Task;
 
 
 namespace WebApplication1.DAL;
 
 public class ExamDbContext : DbContext
 {
+    DbSet<Language> Languages { get; set; }
+    DbSet<Task> Tasks { get; set; }
     protected ExamDbContext()
     {
     }
---
+++ WebApplication1/WebApplication1/Model/Task.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Model;
+
+public class Task
+{
+    [Key]
+    public int IdTask { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+}

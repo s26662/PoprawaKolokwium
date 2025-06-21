4f0a8a2752f0d951d64d427534d0fc794a823d6c
olafc <s26662@pjwstk.edu.pl>
2025-06-21 08:29:53 +0200

    Created project

---
+++ WebApplication1/WebApplication1/DAL/ExamDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace WebApplication1.DAL;
+
+public class ExamDbContext : DbContext
+{
+    protected ExamDbContext()
+    {
+    }
+
+    public ExamDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        //seeds
+    }
+
+}
